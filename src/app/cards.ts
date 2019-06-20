c3711907eee94f580d93d3a8972c6073655123e1
Jake Stothard <stothardj@gmail.com>
2019-06-20 01:25:55 -0700

    Add all cards and use random ones in the game

--- src/app/cards.ts
+++ src/app/cards.ts
@@ -1,5 +1,6 @@
 import { Card } from './card';
 import { Move } from './move';
+import { shuffle } from './random';
 
 interface CardDef {
     left?: number;
@@ -24,7 +25,43 @@ export const CARDS: Record<string, CardDef[]> = {
 	    {'right': 1}],
     'crab': [{'left': 2},
 	     {'up': 1},
-	     {'right': 2}]
+	     {'right': 2}],
+    'cobra': [{'left': 1},
+	      {'right': 1, 'up': 1},
+	      {'right': 1, 'down': 1}],
+    'frog': [{'left': 2},
+	     {'left': 1, 'up': 1},
+	     {'right': 1, 'down': 1}],
+    'goose': [{'left': 1, 'up': 1},
+	      {'left': 1},
+	      {'right': 1},
+	      {'right': 1, 'down': 1}],
+    'rooster': [{'left': 1},
+		{'left': 1, 'down': 1},
+		{'right': 1},
+		{'right': 1, 'up': 1}],
+    'horse': [{'left': 1},
+	      {'up': 1},
+	      {'down': 1}],
+    'crane': [{'left': 1, 'down': 1},
+	      {'up': 1},
+	      {'right': 1, 'down': 1}]
+    'boar': [{'left': 1},
+	     {'up': 1},
+	     {'right': 1}],
+    'elephant': [{'left': 1, 'up': 1},
+		 {'left': 1},
+		 {'right': 1, 'up': 1},
+		 {'right': 1}],
+    'mantis': [{'left': 1, 'up': 1},
+	       {'down': 1},
+	       {'right': 1, 'up': 1}],
+    'dragon': [{'left': 2, 'up': 1},
+	       {'left': 1, 'down': 1},
+	       {'right': 1, 'down': 1},
+	       {'right': 2, 'up': 1}],
+    'tiger': [{'up': 2},
+	      {'down': 1}]
 };
 
 export function getCards(ctx: CanvasRenderingContext2D) {
@@ -38,5 +75,7 @@ export function getCards(ctx: CanvasRenderingContext2D) {
 	}
 	cards.push(new Card(ctx, name, parsedMoves));
     }
-    return cards;
+    shuffle(cards);
+    
+    return cards.slice(0, 5);
 }
